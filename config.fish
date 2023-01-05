c84a5ddf4a4f38b47db62cf7db3707297a3e1b4b
cedric <c.darrou@gmx.com>
2023-01-05 13:56:15 +0100

    add fish alias

---
+++ config.fish
@@ -0,0 +1,7 @@
+#Custom aliases
+alias push='git push https://cedric-darrou:TOKEN>
+alias st='git status'
+alias ga='git add'
+alias gcm='git commit -m'
+alias pull='git pull'
+alias cc='clear ; gcc -Wall -Werror -Wextra -g3 -fsanitize=address'
